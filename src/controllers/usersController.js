8203e3fa8d8152dbe454fdd95b38c9a80a7dff1f
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-04-05 01:44:15 -0300

    feat: implement update user functionality in controller and router

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -14,6 +14,30 @@ const getUserById = (req, res) => {
     res.status(200).json({ id: userId, name, lastName, email, role, createdDate });
 }
 
+const updateUserById = (req, res) => {
+    const { id } = req.params;
+    
+    const user = users.find(user => user.id === parseInt(id));
+
+    if (!user) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const { password } = req.body;
+
+    if (password) {
+        return res.status(400).json({ message: 'No se puede modificar la contraseña aquí' });
+    }
+
+    const { name, lastName, email } = req.body;
+    user.name = name || user.name;
+    user.lastName = lastName || user.lastName;
+    user.email = email || user.email;
+
+    const { id: userId, name: userName, lastName: userLastName, email: userEmail, role, createdDate } = user;
+    res.status(200).json({ id: userId, name: userName, lastName: userLastName, email: userEmail, role, createdDate });
+}
+
 const deleteUserById = (req, res) => {
     const { id } = req.params;
     const user = users.find(user => user.id === parseInt(id));
@@ -43,4 +67,5 @@ module.exports = {
     getAllUsers,
     getUserById,
     deleteUserById,
+    updateUserById,
 };
--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getAllUsers, getUserById, deleteUserById } = require('../controllers/usersController');
+const { getAllUsers, getUserById, deleteUserById, updateUserById } = require('../controllers/usersController');
 
 const usersRouter = Router();
 
@@ -8,5 +8,7 @@ usersRouter.get('/', getAllUsers);
 usersRouter.get('/:id', getUserById);
 
 usersRouter.delete('/:id', deleteUserById);
+
+usersRouter.patch('/:id', updateUserById);
 
 module.exports = usersRouter;
