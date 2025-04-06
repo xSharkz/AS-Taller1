9cec48d15954b7567a2ff3392bfc08eac2d2465c
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-04-06 18:21:53 -0400

    feat: add mongoose and prisma dependencies; update package-lock.json

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-require('dotenv').config();
+const { config } = require('dotenv');
+config({ path: '.env' });
 const express = require('express');
 const morgan = require('morgan');
+const { connect } = require('mongoose');
 const usersRouters = require('./src/routers/usersRouters');
 const authRouters = require('./src/routers/authRouters');
 
 const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || 'development';
+
+const MONGODB = process.env.MONGO_DATABASE
+  .replace('<PASSWORD>', process.env.MONGO_PASSWORD)
+  .replace('<USER>', process.env.MONGO_USER);
+
+connect(MONGODB).then(() => {
+  console.log('Conexión a la base de datos exitosa.');
+}).catch((err) => {
+  console.error('Error al conectar a la base de datos:', err);
+});
 
 const app = express();
 
